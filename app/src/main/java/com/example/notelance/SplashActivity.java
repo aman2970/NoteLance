67ac0e1965ec18a7abebbbee6f714677101e3e2c
AMAN7310 <aman.singh@zypp.app>
2022-11-15 01:05:49 +0530

    splash screen done with code

---
+++ app/src/main/java/com/example/notelance/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.notelance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private int SPLASH_SCREEN_TIME = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME);
+
+    }
+}
