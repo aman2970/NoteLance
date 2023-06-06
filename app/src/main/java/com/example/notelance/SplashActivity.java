02d805c86964e9a1f06752d4595f62287ea0e4da
AMAN7310 <aman.singh@zypp.app>
2023-06-06 18:34:02 +0530

    minor_fixes

--- app/src/main/java/com/example/notelance/SplashActivity.java
+++ app/src/main/java/com/example/notelance/SplashActivity.java
@@ -8,19 +8,16 @@ import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private int SPLASH_SCREEN_TIME = 2000;
+    private int SPLASH_SCREEN_TIME = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         },SPLASH_SCREEN_TIME);
 
     }
